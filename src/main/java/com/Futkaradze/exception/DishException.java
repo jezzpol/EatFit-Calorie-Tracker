ff672d46ad2e8dde049b623bb40e48d2c959dccc
Футкарадзе Валерия <jezzpol@mail.ru>
2026-06-09 19:37:44 +0300

    Реализованы самописаные исключения для Dish и FoodIntake

---
+++ src/main/java/com/Futkaradze/exception/DishException.java
@@ -0,0 +1,9 @@
+package com.Futkaradze.exception;
+
+public class DishException extends Exception {
+
+    public DishException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/Futkaradze/exception/FoodIntakeException.java
@@ -0,0 +1,9 @@
+package com.Futkaradze.exception;
+
+public class FoodIntakeException extends Exception {
+
+    public FoodIntakeException(String message){
+        super(message);
+    }
+
+}
