224c6c77d58ab42ce22df1d8d8a33f2488195001
Футкарадзе Валерия <jezzpol@mail.ru>
2026-06-10 04:00:45 +0300

    Создано дто для FoodInTake

---
+++ src/main/java/com/Futkaradze/requests/FoodIntakeRequest.java
@@ -0,0 +1,14 @@
+package com.Futkaradze.requests;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class FoodIntakeRequest {
+
+    private Long userId;
+    private LocalDate date;
+    private List<Long> dishIds;
+}
